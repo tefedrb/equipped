dd536dab09e8243410c345ff5e6abfc08a1ce44f
tefedrb-enterprise <tefebell@gmail.com>
2020-03-20 23:48:33 -0400

    adding inventory-api service

---
+++ inventory-api/src/main/java/com/example/inventoryapi/InventoryApi.java
@@ -0,0 +1,13 @@
+package com.example.inventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApi.class, args);
+	}
+
+}
---
+++ inventory-api/src/test/java/com/example/inventoryapi/InventoryApiTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
