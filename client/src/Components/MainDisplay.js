7e71fe76e207c5e357ae9a9a799c12f8d7056fc2
tefedrb-enterprise <tefebell@gmail.com>
2020-05-15 11:12:23 -0400

    refactored MainDisplay.js

--- client/src/Components/MainDisplay.js
+++ client/src/Components/MainDisplay.js
@@ -2,14 +2,8 @@ import React from 'react';
 import '../CSS/App.css';
 import styled from 'styled-components';
 
-function MainDisplay(props) {
-  // const createMenuDisplayed = props.showCreateCompMenu ? "dull-area" : null;
-  const companySelect = props.selectedCompany;
-  let joinComp = "";
-  const {userCompany, waitList} = props.userContext.state;
-
-  const Button = styled.button`
-    opacity: ${userCompany || waitList ? ".5" : "1"};
+const Button = styled.button`
+    opacity: ${props => props.userCompany || props.waitList ? ".5" : "1"};
     outline: none;
     height: auto;
     width: auto;
@@ -22,42 +16,44 @@ function MainDisplay(props) {
     transition: box-shadow .1s ease-in-out;
 
     &:hover {
-      ${userCompany || waitList ? "" : "box-shadow: .5px .5px 2.5px #000000"};
-      ${userCompany || waitList ? "" : "background-color: rgba(0,0,0,0.5);"};
+      ${props => props.userCompany || props.waitList ? "" : "box-shadow: .5px .5px 2.5px #000000"};
+      ${props => props.userCompany || props.waitList ? "" : "background-color: rgba(0,0,0,0.5);"};
     }
 
     &:active {
-      ${userCompany || waitList ? "" : "background-color: #69cb42;"}
+      ${props => props.userCompany || props.waitList ? "" : "background-color: #69cb42;"}
     } 
   `
-  // If the user has a WAITLIST# and the SELECTED COMPANY's waitlist
-  // matches the WAITLIST
-  
 
-  if(companySelect){
-    if(waitList !== companySelect.waitList.id){
-      joinComp = 
-        <Button onClick={()=> props.userContext.joinWaitList(companySelect.id)}>
-          Join Company
-        </Button>;
-    } else if(userCompany){
-      joinComp = 
-        <Button>
-          Join Company
-        </Button>;
-    } else if(waitList === companySelect.waitList.id){
-      joinComp = 
-        <Button>
-          On Wait List
-        </Button>; 
-    }
+function MainDisplay(props) {
+  const companyWaitList = props.selectedCompany ? props.selectedCompany.waitList.id : null;
+  const companySelect = props.selectedCompany;
+  const { userCompany, waitList } = props.userContext.state;
+  const { joinWaitList, refreshUser } = props.userContext;
+
+  const joinWaitListThenRefresh = async () => {
+    await joinWaitList(companySelect.id);
+    await refreshUser(localStorage.getItem('jwt'));
+  }
+
+  const buttonOption = (waitList, companyWaitList, userCompany) => {
+    const btnText = userCompany || waitList !== companyWaitList ? "Join Company" : "On Wait List";
+    return (
+      <Button 
+        userCompany={userCompany}
+        waitList={waitList}
+        onClick={waitList !== companyWaitList ? joinWaitListThenRefresh : null}
+      >
+        {btnText}
+      </Button>
+    )
   }
 
   return (
     <div className={`main-display`}>
       <h1>{companySelect ? companySelect.name : ""}</h1>
       <h2>{companySelect ? companySelect.type : ""}</h2>
-      {companySelect ? joinComp : ""}
+      {companySelect ? buttonOption(waitList, companyWaitList, userCompany) : ""}
     </div>
   );
 }
